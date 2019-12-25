a77a426a546115c5a8a49b21fb0e4c05ffb33151
islent <leoislent@gmail.com>
2019-12-25 23:08:25 +0800

    Init setup

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# AstroPlot.jl
---
+++ src/AstroPlot.jl
@@ -0,0 +1,14 @@
+module AstroPlot
+
+using Unitful, UnitfulAstro
+using LaTeXStrings
+using Plotly
+using GR
+using Statistics
+
+using PhysicalParticles
+
+include("RotationCurve.jl")
+include("Density.jl")
+
+end
